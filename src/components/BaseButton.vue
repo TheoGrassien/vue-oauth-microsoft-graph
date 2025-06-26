e2f555967c729a49a87e7d794311892c90011b53
Théo Grassien <theo.grassien@gmail.com>
2025-06-26 15:48:01 +0200

    Ex 7: create BaseButton with primary color

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,36 @@
+<template>
+  <button class="base-button">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: "BaseButton",
+  props: {
+    variant: String,
+  },
+};
+</script>
+
+<style scoped>
+.base-button {
+  padding: 0.5em 1.2em;
+  border: none;
+  border-radius: 4px;
+  background: #26a69a;
+  color: white;
+  font-size: 1rem;
+  transition: background 0.2s;
+}
+
+.base-button:hover {
+  background: #218e83;
+  cursor: pointer;
+}
+
+.base-button:disabled {
+  background: #26a69979;
+  cursor: not-allowed;
+}
+</style> 
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,12 +1,23 @@
 <template>
   <main>
     <h1 class="home-page-title">Bienvenue sur la page d'accueil !</h1>
+    <div class="button-container">
+      <BaseButton class="custom-margin"
+        >BaseButton with custom margin</BaseButton
+      >
+      <BaseButton disabled>BaseButton disabled</BaseButton>
+    </div>
   </main>
 </template>
 
 <script>
+import BaseButton from "../components/BaseButton.vue";
+
 export default {
   name: "HomePage",
+  components: {
+    BaseButton,
+  },
 };
 </script>
 
@@ -14,5 +25,13 @@ export default {
 .home-page-title {
   text-align: center;
   font-size: 1.5rem;
+}
+.custom-margin {
+  margin: 10px;
+}
+.button-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>

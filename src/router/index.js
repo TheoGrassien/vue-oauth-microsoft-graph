76d1e3682fe30d23678f6ac223f1bd6595bfac14
Théo Grassien <theo.grassien@gmail.com>
2025-06-30 23:18:58 +0200

    TP4-Ex 8: add guards for authenticated routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '../pages/HomePage.vue';
 import ConversationsIndexPage from '../pages/ConversationsIndexPage.vue';
+import { useUserStore } from '../lib/userStore.js';
 
 const routes = [
   { path: '/', component: HomePage },
@@ -10,6 +11,15 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const userStore = useUserStore();
+  if (to.path.startsWith('/conversations') && !userStore.user) {
+    next('/');
+  } else {
+    next();
+  }
 });
 
 export default router;
